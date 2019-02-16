27fbd0cc94e8746ceef1605567ddcfb384e12fc4
Maria Luiza Soares <maria.soares@coolblue.nl>
2019-02-16 22:44:11 +0100

    Default page for root endpoint

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@ const readerController = require('./controllers/readerController');
 
 const app = express();
 
+app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
+
 app.get('/parse', readerController.parseUrl);
 
 module.exports = app;
