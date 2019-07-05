26ad9b191f61bd5972409084b1aad207321acf2f
Maria Luiza Soares <maria.soares@coolblue.nl>
2019-07-05 14:57:05 +0200

    test: Second attempt

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const readerController = require('./controllers/readerController');
 const app = express();
 
 Sentry.init({
-  release: 'anyread-api@1.0.0'
+  release: 'a5cc17d885c02358d1e4a875c7c61723a182fb1e'
 });
 
 app.get('/', (req, res) => {
@@ -16,6 +16,10 @@ app.get('/parse', readerController.parseUrl);
 
 app.get('/debug-sentry', (req, res) => {
   throw new Error('My first Sentry error!');
+});
+
+app.get('/debug-sentry-2', (req, res) => {
+  throw new Error('My second Sentry error!');
 });
 
 app.use(Sentry.Handlers.errorHandler());
