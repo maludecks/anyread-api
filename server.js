a5cc17d885c02358d1e4a875c7c61723a182fb1e
Maria Luiza Soares <maria.soares@coolblue.nl>
2019-07-05 14:39:46 +0200

    test: Sentry first try

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+const Sentry = require('@sentry/node');
 const app = require('./src/app');
 
 const port = process.env.PORT || 3000;
 
 app.listen(port);
+
+Sentry.init({ dsn: 'https://3781cd5673024f20a96a7880d77f5bcb@sentry.io/1477852' });
+
+app.use(Sentry.Handlers.requestHandler());
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
--- src/app.js
+++ src/app.js
@@ -1,12 +1,23 @@
+const Sentry = require('@sentry/node');
 const express = require('express');
 const readerController = require('./controllers/readerController');
 
 const app = express();
+
+Sentry.init({
+  release: 'anyread-api@1.0.0'
+});
 
 app.get('/', (req, res) => {
   res.sendfile('index.html');
 });
 
 app.get('/parse', readerController.parseUrl);
+
+app.get('/debug-sentry', (req, res) => {
+  throw new Error('My first Sentry error!');
+});
+
+app.use(Sentry.Handlers.errorHandler());
 
 module.exports = app;
